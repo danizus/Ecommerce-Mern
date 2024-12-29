2e8eb155df36dea3890c66f10122561320e666fe
daniyal <daniyalansari10nov@gmail.com>
2024-12-29 20:33:55 +0500

    pages stepup and route protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,30 +12,60 @@ import ShoppingList from "./pages/shopping-view/listing";
 import NotFound from "./pages/shopping-view/not-found";
 import ShoppingAccount from "./pages/shopping-view/account";
 import ShoppingHome from "./pages/shopping-view/home";
+import CheckAuth from "./components/common/check-auth";
+import UnAuthPage from "./pages/unauth-page";
 
 function App() {
+  const isAuthenticated = true;
+  const user = {
+    name: "john",
+    role: "user",
+  };
   return (
-   <>
-    <Routes>
-      <Route path="/auth" element={<AuthLayout />}>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Route>
+    <>
+      <Routes>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
 
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route path="dashboard" element={<AdminDashboard />} />
-        <Route path="products" element={<AdminProducts />} />
-      </Route>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+        </Route>
 
-      <Route path="/shop" element={<ShoppingLayout />}>
-        <Route path="checkout" element={<ShoppingCheckout />} />
-        <Route path="home" element={<ShoppingHome />} />
-        <Route path="listing" element={<ShoppingList />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="account" element={<ShoppingAccount />} />
-      </Route>
-    </Routes>
-   </>
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingList />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="account" element={<ShoppingAccount />} />
+        </Route>
+
+        <Route path="/unauth-page" element={<UnAuthPage />} />
+      </Routes>
+    </>
   );
 }
 
